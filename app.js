25c899959c9d30e7d03448afcc8f8bc5af1d479f
Gunay Mert Karadogan <mertkaradogan@gmail.com>
2013-05-13 22:28:55 +0200

    browser client, send one message and get the echo.

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var logger = require('winston');
-var argv = require('optimist')
-	.default('hb', true)
-	.argv;
+var argv = require('optimist').argv;
+
 var connectedUsersCount = 0;
 var messagesPerSecond = 0;
+var timeoutLogStatus = 1000;
 
 logger.cli();
 logger.default.transports.console.timestamp = true;
 
-server.listen(80, function(){
+server.listen(8080, function(){
 	console.log("server started");
 });
 
@@ -19,9 +19,13 @@ app.get('/', function (req, res) {
 	res.sendfile(__dirname + '/index.html');
 });
 
-io.set('heartbeats', argv.hb);
-io.set('log level', 1); //0: error, 1:warn, 2:info, 3:debug
+io.set('heartbeat interval', 40);
+// io.set('close timeout', 120);
+//io.set('heartbeat timeout', 60);
+
+io.set('log level', 3); //0: error, 1:warn, 2:info, 3:debug
 io.set('transports', ['websocket']);
+
 
 io.sockets.on('connection', function(socket){
 	connectedUsersCount++;
@@ -36,10 +40,9 @@ io.sockets.on('connection', function(socket){
 	});
 });
 
-
-setTimeout(logStatus, 1000);
+setTimeout(logStatus, timeoutLogStatus);
 function logStatus() {
-    setTimeout(logStatus, 1000);
-    logger.info("users: " + connectedUsersCount + "\tmessagesPerSecond: " + messagesPerSecond);
+    setTimeout(logStatus, timeoutLogStatus);
+    logger.info("users: " + connectedUsersCount + "\tmessagesPerSec: " + messagesPerSecond);
     messagesPerSecond = 0;
 }
