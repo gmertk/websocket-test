b27cd0d6b43a5e46a2bb5ca998ebd9a26241bd47
Gunay Mert Karadogan <mertkaradogan@gmail.com>
2013-06-30 12:13:27 +0200

    before going to sockjs

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ var io = require('socket.io').listen(8080);
 var logger = require('winston');
 var argv = require('optimist').argv;
 var exec = require('child_process').exec;
+var RedisStore = require('socket.io/lib/stores/redis');
+var redis  = require('socket.io/node_modules/redis');
 //require("http").globalAgent.maxSockets = Infinity;
+var redisPort = ;
+var redisUrl = ;
+var pub = redis.createClient(redisPort, redisUrl);
+var sub = redis.createClient(redisPort, redisUrl);
+var store = redis.createClient(redisPort, redisUrl);
+pub.auth('pass', function(){console.log("adentro! pub");});
+sub.auth('pass', function(){console.log("adentro! sub");});
+store.auth('pass', function(){console.log("adentro! store");});
+
 
 //Variables
 var connectedUsersCount = 0;
@@ -11,7 +22,6 @@ var messagesPerSecond = 0;
 var countReceived = 0;
 var timeoutLogStatus = 1000;
 var getCpuCommand = "ps -p " + process.pid + " -o %cpu,%mem";
-console.log(process.pid);
 
 //Logger options
 logger.cli();
@@ -20,11 +30,14 @@ if(argv.o){
   logger.add(logger.transports.File, { filename:  Date.now() + '.txt'});
 }
 //Socket.io options
+//io.disable('heartbeats');
 io.set('heartbeat interval', 40);
 // io.set('close timeout', 120);
 //io.set('heartbeat timeout', 60);
 io.set('log level', 1); //0: error, 1:warn, 2:info, 3:debug
 io.set('transports', ['websocket']);
+io.set('store', new RedisStore({redisPub:pub, redisSub:sub, redisClient:store}));
+
 
 io.sockets.on('connection', function(socket){
 	connectedUsersCount++;
--- client.js
+++ client.js
@@ -3,159 +3,198 @@ var io = require('socket.io-client');
 var _ = require("underscore");
 var fs = require('fs');
 var argv = require('optimist')
-	.usage('Usage: $0 -t [num] -c [num]')
+	.usage('Usage: $0 -t [num newMessageIntervalPerClient] -c [num concurrency]')
     .demand(['t','c'])
     .argv;
 require("http").globalAgent.maxSockets = Infinity;
 
 //Variables
-var amazonServer = "http://ec2-46-51-132-238.eu-west-1.compute.amazonaws.com:8080";
+var amazonServer = "http://ec2-54-216-170-213.eu-west-1.compute.amazonaws.com:80";
 var localServer = "http://localhost:8080";
 var server = argv.l ? localServer : amazonServer;
-var i = 0;
-var clients = [];
-var concurrency = argv.c;
-var rampupTime = 1000;
-var roundTripTimes = [];
 var newMessageIntervalPerClient = argv.t;
-var funcId;
-var timeoutTest = 30000 + rampupTime;
+var concurrency = argv.c;
+var rampupTime = concurrency * 10;
+var testStartTime = rampupTime + 30000;
+var timeoutTest = 30000;
+var waitAfterTest = 10000;
 var isTesting;
-var numberOfDone = 0;
-var numberOfConnected = 0;
-var processRoundtrips = function(){
-	var stats = [];
-	var length = roundTripTimes.length;
-	for (var i = 0; i < length; i++) {
-		stats.push(	roundTripTimes[i].rt );
-	}
-	return stats;
-};
+var numberOfConnected;
+var numberOfFailed;
+var numberOfReconnected;
+var countSentMessages;
+var countReceivedMessaged;
+var roundTripTimes;
+var clients;
+var countId;
 
-var createClient = (function () {
-	var countId = 0;
-	var numberOfFailed = 0;
-	return function(){
-		var id = countId++;
 
-		var socket = io.connect(server, {'force new connection':true});//, 'transports': ['websocket']});
-		var send = function(){
-			var data = {
-				'time' : "",
-				'id' : id,
-				'name': "deviceName",
-				'data' : [
-					{	"current_value" : Math.floor(Math.random() * 31),
-						"id" : "0",
-						"max_value" : "30",
-						"min_value" : "0",
-						"unit": "celcius",
-						"tags": ["heat", "sensor"]
-					},
-					{	"current_value" : Math.floor(Math.random() * 2),
-						"id" : "1",
-						"max_value" : "1",
-						"min_value" : "0",
-						"unit": "",
-						"tags": ["button"]
-					}
-				]
-			};
-			data.time = Date.now();
-			socket.emit('dataMessage', data);
+var createClient = function () {
+	var id = countId++;
+
+	var socket = io.connect(server, {'force new connection':true, 'max reconnection attempts': 0, 'reconnect': false});
+	var send = function(){
+		var data = {
+			'time' : "",
+			'id' : id,
+			'name': "deviceName",
+			'data' : [
+				{	"current_value" : Math.floor(Math.random() * 31),
+					"id" : "0",
+					"max_value" : "30",
+					"min_value" : "0",
+					"unit": "celcius",
+					"tags": ["heat", "sensor"]
+				},
+				{	"current_value" : Math.floor(Math.random() * 2),
+					"id" : "1",
+					"max_value" : "1",
+					"min_value" : "0",
+					"unit": "",
+					"tags": ["button"]
+				}
+			]
 		};
+		data.time = Date.now();
+		socket.emit('dataMessage', data);
+		countSentMessages++;
+	};
 
-		socket.on('connect', function(){
-			numberOfConnected++;
-			var schedule = function(){
-				if(isTesting){
-					send();
-					setTimeout(schedule, newMessageIntervalPerClient);
-				}
-				else{
-					numberOfDone++;
-				}
-			};
-
-			//console.log(id + " connected");
-			schedule();
-
-			socket.on('dataMessage', function(data){
-				var rt = Date.now() - data.time;
-				if(countId >= concurrency-1)
-					roundTripTimes.push({'id': id, 'rt': rt});
-			});
-			socket.on('disconnect', function(){
-				numberOfConnected--;
-				console.log(id + " disconnected!!");
-			});
-
-			socket.on('reconnect_failed', function(){
-				numberOfFailed++;
-				console.log("reconnect_failed for: " + id + ", total failed connections: " + numberOfFailed);
-			});
+	var schedule = function(){
+		if(isTesting){
+			send();
+			setTimeout(schedule, newMessageIntervalPerClient);
+		}
+	};
+	socket.on('connect', function(){
+		numberOfConnected++;
+		socket.customSchedule = schedule;
+		//console.log(id + " connected");
+		socket.on('dataMessage', function(data){
+			countReceivedMessaged++;
+			var rt = Date.now() - data.time;
+			roundTripTimes.push({'id': id, 'rt': rt});
+		});
+		socket.on('disconnect', function(){
+			//console.log(id + " disconnected!!");
+			numberOfFailed++;
 		});
 
+		socket.on('error', function(){
+		});
+		socket.on('reconnect_failed', function(){
+			numberOfReconnected++;
+			console.log("reconnect_failed for: " + id + ", total failed connections: ");
+		});
 		clients.push(socket);
-	};
-}());
+	});
+};
+
 
 var makeConnections = function(conc){
 	var i = 0;
 	var interval = rampupTime / conc;
 	for(i = 0; i < conc; i++){
 		setTimeout(createClient, i * interval);
+		//createClient();
 	}
 };
-var summary = function(arr){
-	var sum = _.reduce(arr, function(memo, num){ return memo + num; }, 0);
-	var average = sum / arr.length;
-	var min = _.min(arr);
-	var max = _.max(arr);
-	return average + "," + min + "," + max;
-};
-var updateMessageInterval = function(){
-	// if(numberOfDone == concurrency - 1){
 
-	// }
+
+var processRoundtrips = function(){
+	var stats = [];
+	var length = roundTripTimes.length;
+	var i;
+	for (i = 0; i < length; i++) {
+		stats.push(	roundTripTimes[i].rt );
+	}
+	return stats;
+};
+
+
+var summary = function(){
+	var allMessagesReceived = (countReceivedMessaged === countSentMessages);
+	var isDropped = numberOfFailed > 0 ? true : false;
+	var success =  (!isDropped && allMessagesReceived) ? true : false;
 	var stats = processRoundtrips();
-	var interval = newMessageIntervalPerClient;
-	var outputSummary = summary(stats);
+	var sum = _.reduce(stats, function(memo, num){ return memo + num; }, 0);
+	var average = sum / stats.length;
+	var min = _.min(stats);
+	var max = _.max(stats);
+	var data = {
+		"concurrency": concurrency,
+		"connected": numberOfConnected,
+		"failed": numberOfFailed,
+		"period": newMessageIntervalPerClient,
+		"average": average,
+		"min": min,
+		"max": max,
+		"countSentMessages": countSentMessages,
+		"countReceivedMessaged": countReceivedMessaged,
+		"success": success
+	};
+	return data;
+};
+
+
+var updateMessageInterval = function(){
+	var output = summary();
 	var fileName = 'test'+ concurrency + '.txt';
-	var data = [concurrency, interval, outputSummary].join(',') + '\n';
-	fs.appendFile(fileName, data, function (err) {
+	var outputString = JSON.stringify(output) + '\n';
+	fs.appendFile(fileName, outputString, function (err) {
 		if(err){
 			console.log(err);
 		}
 		else{
+			console.log(outputString);
 			console.log(fileName + ' written.');
-			process.exit();
+			for(var j= 0; j < clients.length; j++){
+				clients[j].disconnect('unauthorized');
+			}
+			newMessageIntervalPerClient = newMessageIntervalPerClient - 10;
+			if(output.success && newMessageIntervalPerClient > 0){
+				setTimeout(startTest, 30000);
+			}
+			else{
+				process.exit();
+			}
 		}
 	});
 };
+
+
 var finishTest = function(){
 	isTesting = false;
 	console.log("testing finished");
-	setTimeout(updateMessageInterval, 60000);
+	setTimeout(updateMessageInterval, waitAfterTest);
 };
 
 
-(function(){
-	isTesting = true;
+var startTest = function(){
+	countSentMessages = 0;
+	countReceivedMessaged = 0;
+	numberOfFailed = 0;
+	numberOfConnected = 0;
+	numberOfReconnected = 0;
+	numberOfFailed = 0;
+	countId = 0;
+	roundTripTimes = [];
+	clients = [];
+
+	console.log("making connections...");
 	makeConnections(concurrency);
-	setTimeout(finishTest, timeoutTest);
-	// for(var j= 0; j < clients.length; j++){
-	// 	clients[j].disconnect('unauthorized');
-	// }
+	setTimeout(function(){
+		console.log("test started.");
+		isTesting = true;
+		for(var j= 0; j < clients.length; j++){
+			clients[j].customSchedule();
+		}
+		setTimeout(finishTest, timeoutTest);
+	}, testStartTime);
+};
 
-	//console.dir(stats);
-	// for (var key in stats) {
-	// 	if (stats.hasOwnProperty(key)) {
-	// 		var s = stats[key];
-	// 		
-	// 	}
-	// }
-
+(function(){
+	startTest();
 }());
 
 
