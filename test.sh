3a40cae39fcd4d38a0cd846cbb8ef20ab62691cf
Gunay Mert Karadogan <mertkaradogan@gmail.com>
2013-10-22 15:21:22 +0300

    test.h server address as a parameter

--- test.sh
+++ test.sh
@@ -2,13 +2,14 @@
 TIME_SLEEP=60
 INCREASE_PUBLISHER=250
 LIMIT=2000
+SERVER=$1
 
 for ((a=INCREASE_PUBLISHER; a <= LIMIT ; a = a + INCREASE_PUBLISHER))
 do
     sar -o sar$a.sar 1 > /dev/null 2>&1 &
     SAR_PID=$!
     echo "Started with $a publishers"
-    ./load.js -t 0.25 -n $a
+    ./load.js -t 0.25 -n $a -h $SERVER
     echo "Done with $a publishers"
     sleep $TIME_SLEEP
     kill $SAR_PID
